182cf96ccc22a5ccd97c44f2e81eb9e52d6d5d21
AlexJelani <jelanialexander82@gmail.com>
2022-08-05 08:58:24 +0900

    added loadByBreed function

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
 async function start() {
-   const response = await fetch("https://dog.ceo/api/breeds/list/all") 
-   const data = await response.json()
-   //Message contains the list of dog names
-   createBreedList(data.message)
+  const response = await fetch("https://dog.ceo/api/breeds/list/all");
+  const data = await response.json();
+  //Message contains the list of dog names
+  createBreedList(data.message);
 }
-start()
+start();
 //Recieves the data from the message/ can name the parameter anything
 //creates html drop down from data
 function createBreedList(breedList) {
-   //Made a template literal to get breedlist info
-   document.getElementById("breed").innerHTML = `
-   <select>
+  //Made a template literal to get breedlist info
+  //the Object key will return an array called map
+  //made an anonymous function
+  //join to delete the spaces
+  document.getElementById("breed").innerHTML = `
+   <select onchange="loadByBreed(this.value)">
             <option>Choose a breed</option>
-           
+           ${Object.keys(breedList).map(function (breed) {
+            return `<option>${breed}</option>`
+           }).join('')}
 
         </select>
-   `
-    
+   `;
+}
+//made loadByBreed function to populate text with breed information
+function loadByBreed(breed) {
+   alert(breed);
 }
